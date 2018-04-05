cbbf8a801055131220224d7144947cea8c5842e9
Forrest Wilson <forrest.wilson@me.com>
2018-04-05 14:42:45 +1200

    More do while

--- dowhile.php
+++ dowhile.php
@@ -9,8 +9,28 @@
     // Reset $i before next loop
     $i = 1;
 
+    echo "<br>";
+
     while ($i <= 10) {
         echo "$i <br>";
         $i++;
+    }
+
+    // Reset $i before next loop
+    $i = 1;
+
+    echo "<br>";
+
+    while ($i <= 10) {
+        echo "$i <br>";
+        $i++;
+
+        if ($i == 5) {
+            break;
+            echo "This won't print";
+        } else {
+            continue;
+            echo "This sentance will not be printed";
+        }
     }
 ?>
